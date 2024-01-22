94951dedf27ee11a1e675abbd5f19e2b0ed600e3
h3kk4t3h <h3kk4t3h@users.noreply.github.com>
2024-01-22 14:15:02 +0000

    refactor: :construction: changed file names to snake_case, code refractor
    still not passing tests

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,89 +1,101 @@
+# frozen_string_literal: true
+
+# This is the main game class
 class Game
   PLAYER_X = 'X'
   PLAYER_O = 'O'
   EMPTY_CELL = '-'
   BOARD_SIZE = 9
-  LINES = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  LINES = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
 
   def initialize
     @board = Array.new(BOARD_SIZE, EMPTY_CELL)
     @current_player = PLAYER_X
   end
 
-  # Prints the current state of the board
   def print_board
     @board.each_slice(3).with_index do |row, i|
       puts "#{i}  #{row.join(' | ')}"
-      puts "  ---+---+---" unless i == 2
+      puts '  ---+---+---' unless i == 2
     end
   end
 
-  # Makes a move for the current player at the given position
   def make_move(position)
     raise "Invalid move: position #{position} is either out of bounds or already occupied" unless valid_move?(position)
+
     @board[position] = @current_player
-    switch_player unless game_over?
   end
 
-  # Checks if a move is valid
   def valid_move?(position)
-    return false if position < 0 || position >= BOARD_SIZE
+    return false if position.negative? || position >= BOARD_SIZE
     return false if @board[position] != EMPTY_CELL
+
     true
   end
 
-  # Switches the current player
   def switch_player
     @current_player = @current_player == PLAYER_X ? PLAYER_O : PLAYER_X
   end
 
-  # Checks if the game is over (
   def game_over?
-    return switch_player if win?
+    return @current_player == PLAYER_X ? PLAYER_O : PLAYER_X if win?
     return 'Draw' if board_full?
+
     nil
   end
 
- # Controls the flow of the game
-def play
-  result = nil
-  until result
+  def play
+    result = play_turn until result
+    end_game(result)
+  end
+
+  def play_turn
     begin
-      puts "\n\n"
-      print_board
-      puts "\nCurrent player: #{@current_player}"
-      print "Enter a position: "
-      position = gets.chomp.to_i
+      display_turn_info
+      position = get_position
       make_move(position)
       result = game_over?
+      switch_player unless result
     rescue RuntimeError => e
       puts e.message
       retry
     end
+    result
   end
-  switch_player if result != 'Draw'  # Switch back to the winner
-  print_board
-  puts "\nGame Over!"
-  puts "\n"
-  puts "Winner: Player #{result}" unless result == 'Draw'
-  puts "It's a draw!" if result == 'Draw'
-end
+
+  def display_turn_info
+    puts "\n\n"
+    print_board
+    puts "\nCurrent player: #{@current_player}"
+  end
+
+  def get_position
+    print 'Enter a position: '
+    gets.chomp.to_i
+  end
+
+  def end_game(result)
+    print_board
+    puts "\nGame Over!"
+    puts "\n"
+    puts "Winner: Player #{result}" unless result == 'Draw'
+    puts "It's a draw!" if result == 'Draw'
+  end
 
   private
 
-  # Checks if a player has won the game
   def win?
     LINES.any? do |line|
       @board.values_at(*line).uniq.length == 1 && @board[line[0]] != EMPTY_CELL
     end
   end
 
-  # Checks if the board is full
   def board_full?
     @board.none?(EMPTY_CELL)
   end
 end
 
-system "clear" or system "cls"  # Clear the terminal
-puts "Welcome player!"
-Game.new.play
+# Uncomment to play
+# system 'clear' or system 'cls'  # Clear the terminal
+# puts 'Welcome player!'
+# Game.new.play
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,25 +1,24 @@
 # frozen_string_literal: true
 
-require 'rspec'
 require_relative 'spec_helper'
 require_relative '../lib/tic-tac-toe'
 
 describe Game do
-  let(:game) { Game.new }
+  let(:game) { described_class.new }
 
   describe '#valid_move?' do
     it 'returns false if the position is out of bounds' do
-      expect(game.valid_move?(-1)).to eq(false)
-      expect(game.valid_move?(9)).to eq(false)
+      expect(game.valid_move?(-1)).to be false
+      expect(game.valid_move?(9)).to be false
     end
 
     it 'returns false if the position is already occupied' do
       game.make_move(0)
-      expect(game.valid_move?(0)).to eq(false)
+      expect(game.valid_move?(0)).to be false
     end
 
     it 'returns true if the position is within bounds and not occupied' do
-      expect(game.valid_move?(0)).to eq(true)
+      expect(game.valid_move?(0)).to be true
     end
   end
 
@@ -31,27 +30,31 @@ describe Game do
 
     it 'makes a move if the move is valid' do
       game.make_move(0)
-      expect(game.instance_variable_get(:@board)[0]).not_to eq(Game::EMPTY_CELL)
+      expect(game.instance_variable_get(:@board)[0]).not_to eq(described_class::EMPTY_CELL)
     end
   end
 
   describe '#game_over?' do
     it 'returns the winner if there is one' do
-      game.make_move(0)
-      game.make_move(3)
-      game.make_move(1)
-      game.make_move(4)
-      game.make_move(2)
-      expect(game.game_over?).to eq(Game::PLAYER_O)
+      game.make_move(0) # X
+      game.make_move(3) # O
+      game.make_move(1) # X
+      game.make_move(4) # O
+      game.make_move(2) # X
+      expect(game.game_over?).to eq(described_class::PLAYER_X)
     end
 
     it 'returns "Draw" if the board is full and there is no winner' do
-      [0, 1, 2, 3, 4, 6, 5, 7, 8].each { |i| game.make_move(i) }
+      [0, 1, 2, 3, 4, 5, 6, 7, 8].each { |i| game.make_move(i) }
       expect(game.game_over?).to eq('Draw')
     end
+  end
 
-    it 'returns nil if the game is not over' do
-      expect(game.game_over?).to eq(nil)
+  describe '#play' do
+    it 'plays a game' do
+      allow_any_instance_of(described_class).to receive(:gets).and_return('0', '1', '2', '3', '4', '5', '6', '7', '8')
+      game.play
+      expect(game.game_over?).to eq('Draw')
     end
   end
 end
